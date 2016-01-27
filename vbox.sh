4f09f833896973eb4c29ca0d61a9f0afdda58d17
Amine Khaldi <amine.khaldi@reactos.org>
2016-01-27 17:53:40 +0100

    * Maybe?

--- vbox.sh
+++ vbox.sh
@@ -1,6 +1,6 @@
 instance=`pgrep VirtualBox | wc -l`
 if [ "$instance" -eq "0" ]; then
     echo Starting VirtualBox GUI
-    xvfb-run -a /usr/bin/VirtualBox &
+    sudo xvfb-run -a /usr/bin/VirtualBox &
     sleep 10s
 fi
