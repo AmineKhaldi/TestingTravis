38fb3b716e4451623c575630d624841af4f19f6e
Amine Khaldi <amine.khaldi@reactos.org>
2016-01-28 18:54:28 +0100

    * Test.

--- vbox.sh
+++ vbox.sh
@@ -1,5 +1,17 @@
+#!/usr/bin/expect
 spawn /tmp/VMware.bundle --eulas-agreed
 expect "Would you like to check for product updates on startup? \[yes\]:" { send -- "yes\r" }
+#expect "Press Enter to proceed." { send "\r" }
+#set done 0
+#while {$done == 0} {
+#  expect {
+#    "More" { send -- " " }
+#    -exact "Do you agree? \[yes/no\]:" { send -- "yes\r" }
+#    eof { set done 1 }
+#  }
+#}
+
+
 #instance=`pgrep VirtualBox | wc -l`
 #if [ "$instance" -eq "0" ]; then
 #    echo Starting VirtualBox GUI
