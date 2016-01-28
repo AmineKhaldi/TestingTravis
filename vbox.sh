156dbab014883b7dcf0585c763fae5cb0fc5172a
Amine Khaldi <amine.khaldi@reactos.org>
2016-01-28 18:26:20 +0100

    * Test.

--- vbox.sh
+++ vbox.sh
@@ -1,6 +1,8 @@
-instance=`pgrep VirtualBox | wc -l`
-if [ "$instance" -eq "0" ]; then
-    echo Starting VirtualBox GUI
-    sudo xvfb-run -a /usr/bin/VirtualBox &
-    sleep 1s
-fi
+spawn /tmp/VMware.bundle --eulas-agreed
+expect "Would you like to check for product updates on startup? \[yes\]:" { send -- "yes\r" }
+#instance=`pgrep VirtualBox | wc -l`
+#if [ "$instance" -eq "0" ]; then
+#    echo Starting VirtualBox GUI
+#    sudo xvfb-run -a /usr/bin/VirtualBox &
+#    sleep 1s
+#fi
